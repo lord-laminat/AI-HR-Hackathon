9aabf682a19fdf3cc785b0d382c9f85a9e8eafad
lord-laminat <rychkin06@gmail.com>
2025-09-05 05:08:02 +0300

    feat/cv_analysis: little update AnalyseJobService
    Изменена сигнатура конструктора с целью сделать из этого сервиса
    ИИ-агента

--- backend/services/cv_analysis.py
+++ backend/services/cv_analysis.py
@@ -1,3 +1,5 @@
+import os
+
 from schemas.cv_analysis import JobDescription
 from repositories.cv_analysis import JobRepository, JobParameters, VacancyMatchingReport
 
@@ -8,11 +10,22 @@ class AnalyseJobService:
     в вакансии/резюме и формированию ответа пользователю.
     """
 
-    def __init__(self, repository: JobRepository) -> None:
+    def __init__(
+            self,
+            repository: JobRepository,
+            model_name: str,
+            temperature: float
+    ) -> None:
         self.repository = repository
+        self.llm = ChatOpenAI(
+            model=model_name,
+            api_key=os.getenv("OPENROUTER_API_KEY"),
+            base_url="https://openrouter.ai/api/v1",
+            temperature=temperature
+        )
 
 
-    def get_job_parameters(self, job: JobDescription) -> JobParameters:
+    def _get_job_parameters(self, job: JobDescription) -> JobParameters:
         """
         Вызов ИИ-агента для формирования
         описания вакансии/резюме.
@@ -21,7 +34,7 @@ class AnalyseJobService:
         # TODO: Реализовать ИИ-агента, способного анализировать вакансии и резюме.
     
 
-    def compare_resume_with_vacancy(
+    def _compare_resume_with_vacancy(
             self,
             resume: JobParameters,
             vacancy: JobParameters
