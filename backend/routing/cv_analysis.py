4356f29e9719dd61785e41374fd8e68d4d22a62e
lord-laminat <rychkin06@gmail.com>
2025-09-08 15:26:01 +0300

    feat/cv_analysis: add 'get_matching_vacancies' endpoint

--- backend/routing/cv_analysis.py
+++ backend/routing/cv_analysis.py
@@ -3,23 +3,22 @@ from fastapi import APIRouter, Depends
 
 from depends import get_job_service
 from schemas.cv_analysis import VacancyMatchingReport
-from services.cv_analysis import AnalyseJobService
+from services.cv_analysis import CVAnalysisService
 
 router = APIRouter(tags=["CV analysis"])
 
 
 @router.post(
-    "analyse_cv",
+    "/analyse_cv",
     responses={400: {"description": "Bad request"}},
     response_model=VacancyMatchingReport,
     description="Поиск подходящих под резюме вакансий",
 )
-async def get_all_books(
-    book_service: BookService = Depends(get_job_service),
+async def get_matching_vacancies(
+    cv_description: str, cv_analysis_service: CVAnalysisService = Depends(get_job_service)
 ) -> List[VacancyMatchingReport]:
     """
     Ручка принимает резюме соискателя и возвращяет ему
     ответ от сервиса по анализу и поиску подходящих вакансий.
     """
-    ...
-    # TODO: реализовать ручку для поиска вакансий по резюме
+    return cv_analysis_service.analyse(cv_description)
